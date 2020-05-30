2e3f0e44d6daf63691616f57e74ae06a9ed80af2
Dobromir Petrov <luzterin@gmail.com>
2020-05-30 23:34:57 +0300

    final

---
+++ Defining Classes-Exercises-05/DefiningClasses/DateModifier.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace DefiningClasses
+{
+    public class DateModifier
+    {
+        public string CalculateDifference(string dateOne, string dateTwo)
+        {
+
+            int[] dateSplit = dateOne.Split().Select(int.Parse).ToArray();
+            var year = dateSplit[0];
+            var month = dateSplit[1];
+            var day = dateSplit[2];
+            DateTime dateOneDT = new DateTime(year, month, day);
+
+            dateSplit = dateTwo.Split().Select(int.Parse).ToArray();
+            year = dateSplit[0];
+            month = dateSplit[1];
+            day = dateSplit[2];
+            DateTime dateTwoDT = new DateTime(year, month, day);
+            TimeSpan difference = dateTwoDT - dateOneDT;
+
+            return Math.Abs(difference.Days).ToString();
+        }
+    }
+}
---
+++ Defining Classes-Exercises-05/DefiningClasses/StartUp.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main(string[] args)
+        {
+            string dateOne = Console.ReadLine();
+            string dateTwo = Console.ReadLine();
+            DateModifier check = new DateModifier();
+            Console.WriteLine(check.CalculateDifference(dateOne, dateTwo));
+        }
+    }
+}
