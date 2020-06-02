a58869fe2066a7108706fbe89cc86bff2f589d4b
Dobromir Petrov <luzterin@gmail.com>
2020-06-02 21:30:00 +0300

    final

--- Implementing Linked List/Create_Custom_Data_Structures/CoolStack.cs
+++ Implementing Linked List/Create_Custom_Data_Structures/CoolStack.cs
@@ -6,52 +6,52 @@ namespace Create_Custom_Data_Structures
 {
     public class CoolStack<T>
     {
-        private T[] values;
-        private int count;
+        private T[] _values;
+        private int _count;
         public CoolStack(int initialCapacity)
         {
-            this.count = 0;
-            this.values = new T[initialCapacity];
+            this._count = 0;
+            this._values = new T[initialCapacity];
         }
         public CoolStack() : this(4)
         {
         }
-        public int Count { get => this.count; }
+        public int Count { get => this._count; }
 
         public void Push(T value)
         {
-            if (this.count == this.values.Length)
+            if (this._count == this._values.Length)
             {
-                var nextValues = new T[this.values.Length * 2];
-                for (int i = 0; i < this.values.Length; i++)
+                var nextValues = new T[this._values.Length * 2];
+                for (int i = 0; i < this._values.Length; i++)
                 {
-                    nextValues[i] = this.values[i];
+                    nextValues[i] = this._values[i];
                 }
-                this.values = nextValues;
+                this._values = nextValues;
             }
 
-            this.values[this.count] = value;
-            this.count++;
+            this._values[this._count] = value;
+            this._count++;
         }
 
         public object Pop(T value)
         {
-            if (this.count == 0)
+            if (this._count == 0)
             {
                 throw new InvalidOperationException("CoolStack is empty.");
             }
-            var lastIndex = this.count - 1;
-            var last = this.values[lastIndex];
-            this.values[lastIndex] = default;
-            this.count--;
+            var lastIndex = this._count - 1;
+            var last = this._values[lastIndex];
+            this._values[lastIndex] = default;
+            this._count--;
             return last;
         }
 
         public void ForEach(Action<object> action)
         {
-            for (int i = 0; i < this.count; i++)
+            for (int i = 0; i < this._count; i++)
             {
-                action(this.values[i]);
+                action(this._values[i]);
             }
         }
     }
