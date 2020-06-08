3cd79218aa785ef431bdbf533ec4421227188340
Dobromir Petrov <luzterin@gmail.com>
2020-06-08 23:17:55 +0300

    aa

---
+++ ExamPreparation/ClubParty/ClubParty.cs
@@ -0,0 +1,74 @@
+﻿namespace ClubParty
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Text;
+    class ClubParty
+    {
+        static void Main(string[] args)
+        {
+            //60
+            //1 20 b 20 20 a
+
+
+            var reservationCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string[] reservation = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Stack<char> halls = new Stack<char>();
+            Stack<int> reservations = new Stack<int>();
+            char hall;
+            int currentCapacity;
+            for (int i = 0; i < reservation.Length; i++)
+            {
+                if (int.TryParse(reservation[i], out currentCapacity))
+                {
+                    reservations.Push(currentCapacity);
+                }
+                else
+
+                {
+                    hall = char.Parse(reservation[i]);
+                    halls.Push(hall);
+                }
+            }
+
+            StringBuilder sb = new StringBuilder();
+
+            while (halls.Any() && reservations.Any())
+
+            {
+                bool overflow = false;
+                char currentHall = halls.Peek();
+                int currentHallCapacity = reservationCapacity;
+                sb.Append($"{currentHall} -> ");
+
+                while (reservations.Any())
+                {
+                    int currentReservation = reservations.Peek();
+                    int testCapacity = currentHallCapacity - currentReservation;
+                    if (testCapacity >= 0)
+                    {
+                        currentReservation = reservations.Pop();
+                        currentHallCapacity -= currentReservation;
+                        sb.Append($"{currentReservation}, ");
+                    }
+
+                    if (testCapacity <= 0)
+                    {
+                        overflow = true;
+                        halls.Pop();
+                        break;
+                    }
+                }
+
+                if (overflow)
+                {
+                    Console.WriteLine(sb.ToString().TrimEnd(new[] { ',', ' ' }));
+                    sb.Clear();
+                }
+            }
+        }
+    }
+}
---
+++ ExamPreparation/ClubParty_New/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ClubParty_New
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var maxCapacity = int.Parse(Console.ReadLine());
+            string input = Console.ReadLine();
+            string[] reservation = input.Split(' ', StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+            Stack<string> elements = new Stack<string>(reservation);
+            Queue<string> halls = new Queue<string>();
+            List<int> allGroups = new List<int>();
+            int currentCapacity = 0;
+
+            while (elements.Count > 0)
+            {
+                string currentElement = elements.Pop();
+                bool isNumber = int.TryParse(currentElement, out int parsedNumber);
+                if (!isNumber)
+                {
+                    halls.Enqueue(currentElement);
+                }
+                else
+                {
+                    if (halls.Count == 0)
+                    {
+                        continue;
+                    }
+                    if (currentCapacity + parsedNumber > maxCapacity)
+                    {
+                        Console.WriteLine($"{halls.Dequeue()} -> {string.Join(", ", allGroups)}");
+                        allGroups.Clear();
+                        currentCapacity = 0;
+                    }
+
+                    if (halls.Count > 0)
+                    {
+                        allGroups.Add(parsedNumber);
+                        currentCapacity += parsedNumber;
+                    }
+                }
+
+            }
+        }
+    }
+}
