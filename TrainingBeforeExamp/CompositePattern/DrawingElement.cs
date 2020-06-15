0dd1c269edf7e7802741f340a2684580f1d3bb62
Dobromir Petrov <luzterin@gmail.com>
2020-06-15 21:22:39 +0300

    фф

---
+++ TrainingBeforeExamp/CompositePattern/DrawingElement.cs
@@ -0,0 +1,23 @@
+﻿namespace CompositePattern
+{
+    /// <summary>
+    /// The 'Component' Treenode
+    /// </summary>
+
+    abstract class DrawingElement
+
+    {
+        protected string Name;
+
+        // Constructor
+
+        protected DrawingElement(string name)
+        {
+            this.Name = name;
+        }
+
+        public abstract void Add(DrawingElement d);
+        public abstract void Remove(DrawingElement d);
+        public abstract void Display(int indent);
+    }
+}
---
+++ TrainingBeforeExamp/CompositePattern/Program.cs
@@ -0,0 +1,49 @@
+﻿namespace CompositePattern
+{
+    using System;
+    using System.Collections.Generic;
+
+    /// <summary>
+    /// MainApp startup class for Real-World 
+    /// Composite Design Pattern.
+    /// </summary>
+
+    class MainApp
+
+    {
+        /// <summary>
+        /// Entry point into console application.
+        /// </summary>
+        static void Main()
+        {
+            // Create a tree structure 
+
+            var root = new CompositeElement("Picture");
+            root.Add(new PrimitiveElement("Red Line"));
+            root.Add(new PrimitiveElement("Blue Circle"));
+            root.Add(new PrimitiveElement("Green Box"));
+
+            // Create a branch
+
+            var comp = new CompositeElement("Two Circles");
+            comp.Add(new PrimitiveElement("Black Circle"));
+            comp.Add(new PrimitiveElement("White Circle"));
+            root.Add(comp);
+
+            // Add and remove a PrimitiveElement
+
+            PrimitiveElement pe =
+              new PrimitiveElement("Yellow Line");
+            root.Add(pe);
+            root.Remove(pe);
+
+            // Recursively display nodes
+
+            root.Display(1);
+
+            // Wait for user
+
+            Console.ReadKey();
+        }
+    }
+}
